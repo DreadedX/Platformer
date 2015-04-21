00b98a8d287738ab112f65ec53fbb67287deb90c
Dreaded_X <tim.huizinga@gmail.com>
2015-04-21 23:23:18 +0200

    Added launcher which unpacks natives and loads them

--- platformer/src/main/java/com/mtgames/platformer/Launcher.java
+++ platformer/src/main/java/com/mtgames/platformer/Launcher.java
@@ -2,23 +2,97 @@ package com.mtgames.platformer;
 
 import com.mtgames.platformer.debug.Debug;
 
-import java.util.Objects;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Field;
+import java.nio.file.Files;
 
-public class Launcher {
-	public static void main(String[] args) {
-		if (args.length > 0) {
-			Game.scale = Integer.parseInt(args[0]);
-		} else {
-			Game.scale = 2;
+class Launcher {
+	public static void main(String[] args) throws NoSuchFieldException {
+		getNatives();
+
+		System.setProperty("java.library.path", "natives");
+
+		try {
+			Field fieldSysPath = ClassLoader.class.getDeclaredField( "sys_paths" );
+			fieldSysPath.setAccessible(true);
+			fieldSysPath.set(null, null);
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
 		}
 
-		if (args.length > 1) {
-			if (Objects.equals(args[1], "debug")) {
-				Debug.priority = Debug.INFO;
-				Debug.debug = true;
+		Game.main(args);
+	}
+
+	private static void getNatives() {
+		String osName = System.getProperty("os.name");
+		if (osName.contains(" ")) {
+			osName = osName.substring(0, osName.indexOf(' '));
+		}
+		String osArch = System.getProperty("sun.arch.data.model");
+		String libExt = libExt(osName);
+		String libArch = libArch(osArch);
+
+		Debug.log("Detected system: " + osName + "_" + osArch, Debug.LAUNCHER);
+
+		File directory = new File("natives");
+		if(!directory.exists()) {
+			Debug.log("Creating natives folder", Debug.LAUNCHER);
+			if (!directory.mkdir()) {
+				Debug.log("Unable to create natives folder", Debug.ERROR);
 			}
 		}
 
-		new Game().start();
+		extract("natives/libaparapi_" + libArch + "." + libExt);
+
+		directory.deleteOnExit();
+	}
+
+	private static void extract(String location) {
+		File file = new File(location);
+		if (!file.exists()) {
+			Debug.log("Extracting: " + location, Debug.LAUNCHER);
+			InputStream link = (ClassLoader.getSystemResourceAsStream(location));
+			try {
+				Files.copy(link, file.getAbsoluteFile().toPath());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		file.deleteOnExit();
+	}
+
+	private static String libExt(String osName) {
+		switch (osName) {
+			default:
+				Debug.log("Unsupported OS: " + osName, Debug.ERROR);
+				break;
+
+			case "Linux":
+				return "so";
+
+			case "Mac":
+				return "dylib";
+
+			case "Windows":
+				return "dll";
+		}
+		return "";
+	}
+
+	private static String libArch(String osArch) {
+		switch (osArch) {
+			default:
+				Debug.log("Unsupported architecture: " + osArch, Debug.ERROR);
+				break;
+
+			case "64":
+				return "x86_64";
+
+			case "86":
+				return "x86";
+		}
+		return "";
 	}
 }
