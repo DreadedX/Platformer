e59f877f73b5be532d256692567dce779b68eb19
Dreaded_X <tim.huizinga@gmail.com>
2015-07-13 19:01:11 +0200

    Started basics for trigger system

---
+++ platformer/src/main/resources/python/entities/trigger.py
@@ -0,0 +1,21 @@
+from com.mtgames.platformer.scripting.interfaces import EntityInterface
+from com.mtgames.platformer.gfx import Screen
+from com.mtgames.utils import Debug
+
+from com.sun.javafx.geom import Vec4f
+
+class Trigger(EntityInterface):
+    def __init__(self):
+        pass
+
+    def init(self, entity):
+        pass
+
+    def tick(self, entity):
+        if entity.hasCollidedEntity("player"):
+           Debug.log("test", Debug.DEBUG)
+           entity.life = 0
+
+    def render(self, entity):
+        Screen.drawRectangle(entity.x+entity.xMin-Screen.xOffset, entity.y+entity.yMin-Screen.yOffset, entity.x+entity.xMax-Screen.xOffset, entity.y+entity.yMax-Screen.yOffset, Vec4f(1, 1, 1, 1))
+
